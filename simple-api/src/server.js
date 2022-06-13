e0b0f33f458b27b653433bafba409fead64e8b6d
Lucas <lucas@credinvestments.com>
2022-06-13 11:57:01 -0300

    main

---
+++ simple-api/src/server.js
@@ -0,0 +1,16 @@
+import http from "http";
+const processId = process.pid;
+
+const server = http.createServer((request, response) => {
+  for (let index = 0; index < 1e7; index++);
+  response.end(`handled by pid: ${processId}`);
+});
+server.listen(3000).once("listening", () => {
+  console.log("Server started in process", processId);
+});
+
+// Graceful shutdown (wait connections be closed to close the app)
+process.on("SIGTERM", () => {
+  console.log("server ending", new Date().toISOString());
+  server.close(() => process.exit());
+});
